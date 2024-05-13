ed84632431e38fe1e3d301961c73192f48b3c40c
type <type>
2024-05-13 08:09:41 -0700

    Updating OpenGLTexture with Image.h

--- Engine3D/OpenGL/OpenGLTexture.h
+++ Engine3D/OpenGL/OpenGLTexture.h
@@ -2,6 +2,7 @@
 #include <Engine3D/Graphics/Texture.h>
 #include <cstdint>
 #include <string>
+#include <Engine3D/Image/Image.h>
 
 namespace Engine3D{
 
@@ -14,10 +15,12 @@ namespace Engine3D{
         virtual ~OpenGLTexture2D();
 
     private:
-		uint32_t GetRendererIDInternal() const override { return id; }
+		uint32_t GetRendererIDInternal() const override;
 
-        uint32_t GetWidthInternal() const override { return width; }
-        uint32_t GetHeightInternal() const override { return height; }
+        uint32_t GetWidthInternal() const override;
+        uint32_t GetHeightInternal() const override;
+
+        bool IsTextureLoadedSuccessful() const override;
 
 		void SetDataInternal(void* data, uint32_t size) override;
 
@@ -29,63 +32,13 @@ namespace Engine3D{
 
     private:
         std::string filepath;
-        uint32_t width, height;
+        Image image;
+        int width, height;
+
+        // Ref<Image> image;
         uint32_t id;
 		GLenum internalFormat, formatData;
-    };
-
-
-    class OpenGLTexture3D : public Texture3D {
-    public:
-        OpenGLTexture3D(uint32_t width, uint32_t height);
-        OpenGLTexture3D(const std::string& filepath);
-
-        uint32_t GetRendererIDInternal() const override { return id; }
-
-        uint32_t GetWidthInternal() const override { return width; }
-        uint32_t GetHeightInternal() const override { return height; }
-
-		void SetDataInternal(void* data, uint32_t size) override;
-
-        void bind(GLenum bind = GL_TEXTURE_3D) const override;
-
-		bool operator==(const Texture3D& other) const override{
-			return id == ((OpenGLTexture3D&)other).id;
-		}
-
-    private:
-        std::string filepath;
-        uint32_t width;
-        uint32_t height;
-        uint32_t id;
-		GLenum internalFormat, formatData;
-    };
-
-    template<GLenum type>
-    class OpenGLCustomizedTexture : public CustomizedTexture<type> {
-    public:
-        OpenGLCustomizedTexture(uint32_t width, uint32_t height);
-        OpenGLCustomizedTexture(const std::string& filepath);
-
-        uint32_t GetRendererIDInternal() const override { return id; }
-
-        uint32_t GetWidthInternal() const override { return width; }
-        uint32_t GetHeightInternal() const override { return height; }
-
-		void SetDataInternal(void* data, uint32_t size) override;
-
-        void bind(GLenum bind = GL_TEXTURE_3D) const override;
-
-		bool operator==(const CustomizedTexture<type>& other) const override{
-			return id == ((OpenGLCustomizedTexture<type>&)other).id;
-		}
-
-    private:
-        std::string filepath;
-        uint32_t width;
-        uint32_t height;
-        uint32_t id;
-		GLenum internalFormat, formatData;
+        bool isTextureLoaded = false;
     };
 
 };
--- src/Engine3D/OpenGL/OpenGLTexture.cpp
+++ src/Engine3D/OpenGL/OpenGLTexture.cpp
@@ -1,9 +1,11 @@
 #include <Engine3D/Engine3DPrecompiledHeader.h>
 #include <OpenGL/OpenGLTexture.h>
+// #include <Engine3D/Image/stb_image.h>
 
 namespace Engine3D{
-	OpenGLTexture2D::OpenGLTexture2D(uint32_t w, uint32_t h) : width(w), height(h) {
+	OpenGLTexture2D::OpenGLTexture2D(uint32_t w, uint32_t h) : width(w), height(h), image(w, h){
 		RENDER_PROFILE_FUNCTION();
+        image.SetVerticalOnLoad(true);
 		
         // Uploading data to OpenGL texture
 		// internalData is our internalFormat
@@ -19,43 +21,59 @@ namespace Engine3D{
 
 	}
 
-    OpenGLTexture2D::OpenGLTexture2D(const std::string& filepath) : filepath(filepath) {
+    OpenGLTexture2D::OpenGLTexture2D(const std::string& filepath) : image(filepath) {
 		RENDER_PROFILE_FUNCTION();
+        image.SetVerticalOnLoad(true);
 
-        coreLogInfo("OpenGLTexture2D Create Called!");
-        int imageWidth, imageHeight, channels;
+        // coreLogInfo("OpenGLTexture2D Create Called!");
+        // // image.SetVerticalOnLoad(true);
+        // int imageWidth, imageHeight, channels;
 
-        stbi_set_flip_vertically_on_load(1);
-		stbi_uc* data = nullptr;
-		{
-		RENDER_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
-        // Loading in our image data
-        data = stbi_load(filepath.c_str(), &imageWidth, &imageHeight, &channels, 0);
-		}
+        // stbi_set_flip_vertically_on_load(1);
+		// stbi_uc* data = nullptr;
+		// {
+		// RENDER_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
+        // // Loading in our image data
+        // data = stbi_load(filepath.c_str(), &imageWidth, &imageHeight, &channels, 0);
+		// }
 
-        if(data == nullptr){
-            coreLogError("Failed to load image as the value was nullptr!");
-        }
-        else coreLogInfo("Image Data was not nullptr meaning it was valid!");
-        // render_core_assert(data, "Failed to load image!");
+        // if(data == nullptr){
+        //     coreLogError("Failed to load image as the value was nullptr!");
+        //     isTextureLoaded = false;
+        //     assert(false);
+        // }
+        // else isTextureLoaded = true;
+        // // else coreLogInfo("Image Data was not nullptr meaning it was valid!");
+        // // render_core_assert(data, "Failed to load image!");
 
-        width = imageWidth;
-        height = imageHeight;
+        // width = imageWidth;
+        // height = imageHeight;
 
         // Uploading data to OpenGL texture
 		// internalData is our internalFormat
+        // width = image.GetWidth(), height = image.GetHeight();
+
+        // if(!image.IsLoaded()){
+        // if(data == nullptr){
+        //     coreLogWarn("Did not load texture image!");
+        // }
         GLenum internalData = 0, formatData = 0;
+        int channels = image.GetPixelChannels();
+        width = image.GetWidth();
+        height = image.GetHeight();
 
         if(channels == 4){
-            internalData = GL_RGBA8;
+            // internalData = GL_RGBA8;
+            // formatData = GL_RGBA;
+            internalFormat = GL_RGBA8;
             formatData = GL_RGBA;
         }
         else if(channels == 3){
-            internalData = GL_RGB8;
+            // internalData = GL_RGB8;
+            // formatData = GL_RGB;
+            internalFormat = GL_RGB8;
             formatData = GL_RGB;
         }
-
-		internalFormat = internalData, this->formatData = formatData;
 
         // Testing textures for OpenGL
         glGenTextures(1, &id); // Equivalent to glCreateTexture (but will segfault though)
@@ -72,12 +90,10 @@ namespace Engine3D{
 
 		/* glTextureParameteri(id, GL_TEXTURE_WRAP_S, GL_REPEAT); */
 		/* glTextureParameteri(id, GL_TEXTURE_WRAP_T, GL_REPEAT); */
-
-        glTexImage2D(GL_TEXTURE_2D, 0, internalData, width, height, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
+        // glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, imageWidth, imageHeight, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
+        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, formatData, GL_UNSIGNED_BYTE, image.GetImageData()); // same thing as doing: glTextureSubImage2D
 
         glGenerateMipmap(GL_TEXTURE_2D);
-
-		stbi_image_free(data); // free image data stored in CPU
 
         glBindTexture(GL_TEXTURE_2D, 0);
     }
@@ -88,12 +104,14 @@ namespace Engine3D{
         glDeleteTextures(1, &id);
     }
 
-
+    uint32_t OpenGLTexture2D::GetRendererIDInternal() const { return id; }
+    uint32_t OpenGLTexture2D::GetWidthInternal() const { return width; }
+    uint32_t OpenGLTexture2D::GetHeightInternal() const { return height; }
 
 	void OpenGLTexture2D::SetDataInternal(void* data, uint32_t size){
 		RENDER_PROFILE_FUNCTION();
 
-		/* glTextureSubImage2D(id, 0, 0, 0, _width, _height, */ 
+		/* glTextureSubImage2D(id, 0, 0, 0, _width, _height, */
 		
 		// bytes per pixel
 		uint32_t bpp = formatData == GL_RGBA ? 4 : 3;
@@ -113,212 +131,6 @@ namespace Engine3D{
         glBindTexture(GL_TEXTURE_2D, id); // Instead of using this we have to use glBindTexture on Mac
     }
 
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    // Texture3D
-    ///////////////////////////////////////////////
-
-    OpenGLTexture3D::OpenGLTexture3D(uint32_t w, uint32_t h) : width(w), height(h){
-        internalFormat = GL_RGBA8;
-        formatData = GL_RGBA;
-
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		
-		// Since we segfault using glTextureParameteri, we'll use this for now
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    }
-
-    OpenGLTexture3D::OpenGLTexture3D(const std::string& filename) : filepath(filename) {
-        RENDER_PROFILE_FUNCTION();
-
-        coreLogInfo("OpenGLTexture2D Create Called!");
-        int imageWidth, imageHeight, channels;
-
-        stbi_set_flip_vertically_on_load(1);
-		stbi_uc* data = nullptr;
-		{
-		RENDER_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
-        // Loading in our image data
-        data = stbi_load(filepath.c_str(), &imageWidth, &imageHeight, &channels, 0);
-		}
-
-        if(data == nullptr){
-            coreLogError("Failed to load image as the value was nullptr!");
-        }
-        else coreLogInfo("Image Data was not nullptr meaning it was valid!");
-        // render_core_assert(data, "Failed to load image!");
-
-        width = imageWidth;
-        height = imageHeight;
-
-        // Uploading data to OpenGL texture
-		// internalData is our internalFormat
-        GLenum internalData = 0, formatData = 0;
-
-        if(channels == 4){
-            internalData = GL_RGBA8;
-            formatData = GL_RGBA;
-        }
-        else if(channels == 3){
-            internalData = GL_RGB8;
-            formatData = GL_RGB;
-        }
-
-		internalFormat = internalData, this->formatData = formatData;
-
-        // Testing textures for OpenGL
-        glGenTextures(1, &id); // Equivalent to glCreateTexture (but will segfault though)
-        glActiveTexture(GL_TEXTURE_3D);
-        glBindTexture(GL_TEXTURE_3D, id);
-		// /* glCreateTextures(GL_TEXTURE_3D, 1, &_rendererID); */
-		// /* glTextureStorage2D(GL_TEXTURE_3D, 0, _internalFormat, _width, _height); */
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		
-		// Since we segfault using glTextureParameteri, we'll use this for now
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-		/* glTextureParameteri(_rendererID, GL_TEXTURE_WRAP_S, GL_REPEAT); */
-		/* glTextureParameteri(_rendererID, GL_TEXTURE_WRAP_T, GL_REPEAT); */
-
-        glTexImage2D(GL_TEXTURE_3D, 0, internalData, width, height, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
-
-        glGenerateMipmap(GL_TEXTURE_3D);
-
-		stbi_image_free(data); // free image data stored in CPU
-
-        glBindTexture(GL_TEXTURE_3D, 0);
-    }
-
-    void OpenGLTexture3D::bind(GLenum slot) const{
-        RENDER_PROFILE_FUNCTION();
-
-        /* glBindTextureUnit(slot, _rendererID); // Equivalent to glBindTexture (but will segfault though) */
-
-		// Since we are using OpenGL 4.5 or lower, and cannot use glBindTextureUnit, we have to do the following two lines below
-		// Essentially how textures are working with the batch renderer system, now.
-		glActiveTexture(GL_TEXTURE0 + slot);
-        glBindTexture(GL_TEXTURE_3D, id); // Instead of using this we have to use glBindTexture on Mac
-    }
-
-    void OpenGLTexture3D::SetDataInternal(void* data, uint32_t size){
-        /* glTextureSubImage2D(_rendererID, 0, 0, 0, _width, _height, */ 
-		
-		// bytes per pixel
-		uint32_t bpp = formatData == GL_RGBA ? 4 : 3;
-		// render_core_assert(size == _width * _height * bpp, "Data must be an entire texture"); // Make sure that the textures are fine for rn
-        glTexImage2D(GL_TEXTURE_3D, 0, internalFormat, width, height, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
-    }
-
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    ///////////////////////////////////////////////
-    // Customizable Texture
-    ///////////////////////////////////////////////
-    template<GLenum T>
-    OpenGLCustomizedTexture<T>::OpenGLCustomizedTexture(uint32_t w, uint32_t h) : width(w), height(h){
-        internalFormat = GL_RGBA8;
-        formatData = GL_RGBA;
-
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		
-		// Since we segfault using glTextureParameteri, we'll use this for now
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    }
-
-    template<GLenum T>
-    OpenGLCustomizedTexture<T>::OpenGLCustomizedTexture(const std::string& filename) : filepath(filename) {
-        RENDER_PROFILE_FUNCTION();
-
-        coreLogInfo("OpenGLTexture2D Create Called!");
-        int imageWidth, imageHeight, channels;
-
-        stbi_set_flip_vertically_on_load(1);
-		stbi_uc* data = nullptr;
-		{
-		RENDER_PROFILE_SCOPE("stbi_load - OpenGLTexture2D::OpenGLTexture2D(const std::string&)");
-        // Loading in our image data
-        data = stbi_load(filepath.c_str(), &imageWidth, &imageHeight, &channels, 0);
-		}
-
-        if(data == nullptr){
-            coreLogError("Failed to load image as the value was nullptr!");
-        }
-        else coreLogInfo("Image Data was not nullptr meaning it was valid!");
-        // render_core_assert(data, "Failed to load image!");
-
-        width = imageWidth;
-        height = imageHeight;
-
-        // Uploading data to OpenGL texture
-		// internalData is our internalFormat
-        GLenum internalData = 0, formatData = 0;
-
-        if(channels == 4){
-            internalData = GL_RGBA8;
-            formatData = GL_RGBA;
-        }
-        else if(channels == 3){
-            internalData = GL_RGB8;
-            formatData = GL_RGB;
-        }
-
-		internalFormat = internalData, this->formatData = formatData;
-
-        // Testing textures for OpenGL
-        glGenTextures(1, &id); // Equivalent to glCreateTexture (but will segfault though)
-        glActiveTexture(GL_TEXTURE_3D);
-        glBindTexture(GL_TEXTURE_3D, id);
-		// /* glCreateTextures(GL_TEXTURE_3D, 1, &_rendererID); */
-		// /* glTextureStorage2D(GL_TEXTURE_3D, 0, _internalFormat, _width, _height); */
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		
-		// Since we segfault using glTextureParameteri, we'll use this for now
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-		/* glTextureParameteri(_rendererID, GL_TEXTURE_WRAP_S, GL_REPEAT); */
-		/* glTextureParameteri(_rendererID, GL_TEXTURE_WRAP_T, GL_REPEAT); */
-
-        glTexImage2D(GL_TEXTURE_3D, 0, internalData, width, height, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
-
-        glGenerateMipmap(GL_TEXTURE_3D);
-
-		stbi_image_free(data); // free image data stored in CPU
-
-        glBindTexture(GL_TEXTURE_3D, 0);
-    }
-
-    template<GLenum T>
-    void OpenGLCustomizedTexture<T>::bind(GLenum slot) const{
-        RENDER_PROFILE_FUNCTION();
-
-        /* glBindTextureUnit(slot, _rendererID); // Equivalent to glBindTexture (but will segfault though) */
-
-		// Since we are using OpenGL 4.5 or lower, and cannot use glBindTextureUnit, we have to do the following two lines below
-		// Essentially how textures are working with the batch renderer system, now.
-		glActiveTexture(GL_TEXTURE0 + slot);
-        glBindTexture(GL_TEXTURE_3D, id); // Instead of using this we have to use glBindTexture on Mac
-    }
-
-    template<GLenum T>
-    void OpenGLCustomizedTexture<T>::SetDataInternal(void* data, uint32_t size){
-        /* glTextureSubImage2D(_rendererID, 0, 0, 0, _width, _height, */ 
-		
-		// bytes per pixel
-		uint32_t bpp = formatData == GL_RGBA ? 4 : 3;
-		// render_core_assert(size == _width * _height * bpp, "Data must be an entire texture"); // Make sure that the textures are fine for rn
-        glTexImage2D(GL_TEXTURE_3D, 0, internalFormat, width, height, 0, formatData, GL_UNSIGNED_BYTE, data); // same thing as doing: glTextureSubImage2D
-    }
-
+    bool OpenGLTexture2D::IsTextureLoadedSuccessful() const { return isTextureLoaded; }
 
 };
